214c492977f01438c49c1faf1ac865eb05ef19d4
Skipper <60583875+Skipper2197@users.noreply.github.com>
2021-08-25 14:43:38 -0500

    editing ceiling light

--- CeillingLight.gd
+++ CeillingLight.gd
@@ -3,6 +3,10 @@ extends StaticBody2D
 onready var sprite = $Sprite
 
 var on = false
+var exploded = false
+var light_offset = Vector2(-1, 3)
+var clicked = 0
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,10 +19,44 @@ func _ready():
 #	pass
 
 func _input_event(viewport, event, shape_idx):
+
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+		if not exploded:
+			if not on:
+				on()
+				clicked += 1
+				on = true
+			else:
+				off()
+				clicked += 1
+				on = false
+		elif clicked > 5:
+			exploded = true
+			flicker_uncontrollably()
+			explode()
+			
+		
+			
+func on():
+	sprite.region_rect.position.y = 600
+	sprite.position += light_offset
+	
+func off():
+	sprite.region_rect.position.y = -2
+	sprite.position -= light_offset
+			
+func flicker_uncontrollably():
+	var interval = 0.02
+	for i in range(20):
+		yield(get_tree().create_timer(rand_range(.05, .1)), "timeout")
 		if not on:
-			sprite.region_rect.position.y = 520
+			on()
 			on = true
 		else:
-			sprite.region_rect.position.y = 70
+			off()
 			on = false
+			
+			
+func explode():
+	sprite.region_rect.position.y = 50
+	sprite.position -= light_offset
