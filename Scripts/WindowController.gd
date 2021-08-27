94903ec6786e9d42bd7a576ef81690ff3612b180
Wowe-Peanut <47678647+Wowe-Peanut@users.noreply.github.com>
2021-08-27 14:16:33 -0500

    Ball can Break window

---
+++ Scripts/WindowController.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+
+onready var sprite = $Sprite
+
+func _ready():
+	sprite.region_rect = Rect2(114, 563, 538, 382)
+
+func _on_Window_body_entered(body):
+	if body.name == "Ball" && body.linear_velocity.length() > 1000:
+		sprite.region_rect = Rect2(112, 100, 567, 382)
+		body.queue_free()
+		pass
